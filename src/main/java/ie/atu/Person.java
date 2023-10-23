62ccc3b9cda01b4cac1714e3c6f9d7e007145a64
G00425238@atu.ie <G00425238@atu.ie>
2023-10-23 09:37:46 +0100

    Added new class called Person

---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,16 @@
+package ie.atu;
+
+public class Person {
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+}
